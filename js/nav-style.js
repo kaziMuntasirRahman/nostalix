348908536ba471c4fb0f825e077ce881fa68e87e
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-03 00:40:46 +0600

    nav links title fixed, Alhamdulillah

--- js/nav-style.js
+++ js/nav-style.js
@@ -1,11 +1,11 @@
-const navLinkImages = ["images/icons/home.png", "images/icons/edit.png", "images/icons/add-person.png", "images/icons/flag.png"];
+const navLinkImages = ['home', 'edit', 'add-person', 'flag'];
 
 const navLinkContainer = document.getElementById('nav-link-container');
 
 for (const i of navLinkImages) {
   navLinkContainer.innerHTML += `
-  <div class="px-4 py-4 bg-slate-900 bg-opacity-5 rounded-full border border-gray-950 border-opacity-15 justify-start items-center gap-2 inline-flex">
-  <img src=${i}>
+  <div class="px-4 py-4 bg-slate-900 bg-opacity-5 rounded-full border border-gray-950 border-opacity-15 justify-start items-center gap-2 inline-flex cursor-pointer" title="${i}">
+  <img src="images/icons/${i}.png">
 </div>`
 }
 
