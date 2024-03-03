f35a642dd974aef3c21015e8ed871f3dad0de3eb
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-03 18:05:46 +0600

    little responsive correction

--- js/dynamic-comment.js
+++ js/dynamic-comment.js
@@ -9,7 +9,7 @@ function addComment(img){
   document.getElementById('msg-read-counter').innerHTML = msgReadCounter;
   commentContainer.innerHTML += `
   <div class="bg-white rounded-2xl flex items-center gap-2 px-4 py-[15px]">
-  <div class="lg:w-[212px] text-slate-900 text-base font-semibold font-['Mulish'] leading-relaxed mr-auto">${authorTitle}</div>
+  <div class="text-slate-900 text-base font-semibold font-['Mulish'] leading-relaxed mr-auto">${authorTitle}</div>
   <img src="images/icons/view.png" class=""></img>
   <div class="text-slate-900 text-opacity-60 font-['Inter']">${authorView}</div>
 </div>`
